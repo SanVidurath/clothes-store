d606ea681423839cc69c77e5082ea7f6b38d8818
SanVidurath <110455292+SanVidurath@users.noreply.github.com>
2024-06-03 15:28:55 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,7 +4,7 @@ import getProducts from '../controllers/productControllers.js';
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.json({mssg: "hello"})
+    res.json("hello")
 })
 
 router.get("/products", getProducts)
