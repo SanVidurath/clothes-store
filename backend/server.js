567ccc0cd458ff722209d8967c80ae1238ea63d0
SanVidurath <110455292+SanVidurath@users.noreply.github.com>
2024-06-03 16:43:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://clothes-store-client.vercel.app/'
+    origin: 'https://clothes-store-client.vercel.app'
   }));
 
 // routes
